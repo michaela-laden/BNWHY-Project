31f4ba461b0d64ae473844cb50f041629c3c7a33
Michaela Laden <michaela.laden@gmail.com>
2020-02-02 15:20:51 -0500

    serve html and static files

--- bnwhy/settings.py
+++ bnwhy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bnwhy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['frontend/html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'bnwhy.wsgi.application'
 
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend','static')
+    
+]
