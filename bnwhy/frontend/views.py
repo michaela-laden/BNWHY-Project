caff05c366fbe5ca3a7b30497a57730bc5bca00f
Michaela Laden <michaela.laden@gmail.com>
2020-02-04 17:19:14 -0500

    added frontend to forum

--- bnwhy/frontend/views.py
+++ bnwhy/frontend/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import View
 
+
+posts = [
+    {'author' : 'Michaela', 
+     'title' : 'Question 1', 
+     'content' : 'first post',
+     'date': 'January 1, 2020'
+    }
+]
+
+
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'index.html',{})
+
+def alteryx(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'Alteryx.html',context)
+
     
 
