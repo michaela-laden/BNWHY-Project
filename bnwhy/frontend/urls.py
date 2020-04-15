cdf749e74f6db358798a25475bab64471cfb7658
Michaela Laden <michaela.laden@gmail.com>
2020-04-14 21:14:36 -0400

    implement pagination and ability to filter by author

--- bnwhy/frontend/urls.py
+++ bnwhy/frontend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('alteryx', PostListView.as_view(), name='alteryx'),
+    path('alteryx/', PostListView.as_view(), name='alteryx'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='posts-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='posts-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='posts-delete'),
--- bnwhy/frontend/views.py
+++ bnwhy/frontend/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from bnwhy.api.models import Post
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -20,6 +21,17 @@ class PostListView(ListView):
     template_name = 'Alteryx.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'user_post.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
